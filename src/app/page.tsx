7c8c8524b28b094a8507be46cd7cfd6a663bfa36
Richard Vanbergen <rich.vanbergen@gmail.com>
2024-09-02 21:56:43 +0900

    adding simple text page

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+import Link from "next/link";
+import * as React from 'react'
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <>
+      <Link />
+      <Image />
+    </>
+  );
+}
